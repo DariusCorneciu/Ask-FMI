14ca24fc114f86f8d1c338ce6d8174205fae5db4
RaresTudur <RaresTudur@users.noreply.github.com>
2023-12-11 13:57:10 +0200

    Adaugare Model-Categorie

---
+++ Project DAW/Models/Categorie.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_DAW.Models
+{
+    public class Categorie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Titlul categoriei este necesar!!!")]
+        public string Name { get; set; }
+    }
+}
