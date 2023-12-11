c1fdbf6edd37f441cbba61976d49cb07a7ab0c45
RaresTudur <RaresTudur@users.noreply.github.com>
2023-12-11 14:00:09 +0200

    Adaugare Model - SubCategorie

---
+++ Project DAW/Models/SubCategorie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_DAW.Models
+{
+    public class SubCategorie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int CategorieId { get; set;}
+
+        public virtual Categorie Categorie { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+    }
+}
