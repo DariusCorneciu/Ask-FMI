2a2ff6a817211a9cde1f6812544b8acb231d88b3
RaresTudur <RaresTudur@users.noreply.github.com>
2023-12-11 13:50:12 +0200

    Adaugare Model - Intrebare

---
+++ Project DAW/Models/Intrebare.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_DAW.Models
+{
+    public class Intrebare
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Intrebarea trebuie sa fie adaugata!!!!")]
+        public string Continut { get; set; }
+        public DateTime Date { get; set; }
+
+        public bool IsOpen { get; set; }
+    }
+}
