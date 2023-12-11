20bb9d7a377c4caf79262b13fb20cb684b40061e
RaresTudur <RaresTudur@users.noreply.github.com>
2023-12-12 00:46:06 +0200

    Adaugare Controller Intrebare

--- Project DAW/Models/Intrebare.cs
+++ Project DAW/Models/Intrebare.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Project_DAW.Models
 {
@@ -11,11 +13,19 @@ namespace Project_DAW.Models
         public string Continut { get; set; }
         public DateTime Date { get; set; }
         
-        public int SubCategorieId { get; set; }
+        public int? SubCategorieId { get; set; }
         public bool IsOpen { get; set; }
         public virtual SubCategorie? SubCategorie { get; set; }
 
         public virtual ICollection<Comentariu>? Comentarii { get; set; }
+
+        [NotMapped]
+        public IEnumerable<SelectListItem>? SubCateg { get; set; }
+        public string? UserId { get; set; }
+
+        public virtual ApplicationUser? User { get; set; }
+
+
         public virtual Raspuns? Raspuns { get; set; }
        
     }
