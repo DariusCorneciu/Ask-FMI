e9631bbd468429df5ac05aba5087bd11e9b3f2ad
Darius Corneciu <darius-stefan.corneciu@s.unibuc.ro>
2023-12-11 13:48:24 +0200

    model raspuns

---
+++ Project DAW/Models/Raspuns.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_DAW.Models
+{
+    public class Raspuns
+    {
+        public int Id;
+        public int IntrebareId;
+        public int ModeratorId;
+        public DateTime Date;
+        public string Text;
+    }
+}
